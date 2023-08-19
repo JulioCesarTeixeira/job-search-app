bd5f5890e40df43531c0bb90e6d4860cff092655
JulioCesarTeixeira <juliocesarteixeira1992@gmail.com>
2023-08-19 17:17:41 +0200

    eslint rules adjustment and small cleanup

--- src/playground.ts
+++
@@ -1,9 +0,0 @@
-export function evenOrOdd(number: number) {
-  if (number % 2 === 0) {
-    return 'Even'
-  } else {
-    return 'Odd'
-  }
-}
-
-export const multiply = (first: number, second: number) => first * second
